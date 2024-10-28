e9d952cfa3b21b1b1540bdb5b8f0ecdc31273f76
Antony Perez <antonyperez0@yahoo.com>
2024-10-28 11:11:20 -0700

    all tests are passing

---
+++ classes/Cart.js
@@ -0,0 +1,23 @@
+const Product = require("./Product");
+
+class Cart {
+  constructor() {
+    this.products = [];
+    this.total = 0;
+  }
+
+  addProduct(product) {
+    this.products.push(product);
+    this.total += product.price;
+  }
+
+  removeProduct(index) {
+    if (index < 0 || index >= this.products.length) return;
+
+    const product = this.products[index];
+    this.total -= product.price;
+    this.products.splice(index, 1);
+  }
+}
+
+module.exports = Cart;
